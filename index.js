d4ab4738ef279bafc084e9e68ec742d0a2646ec2
mostafij163 <mostafijurrahman163@gmail.com>
2022-03-30 12:55:25 +0600

    add total sales circle on map

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const g = svg.append("g");
 
 const projection = d3.geoMercator().scale(4000).translate([-5850, 2000]);
 const pathGenerator = d3.geoPath().projection(projection);
+const radiusScale = d3.scaleSqrt();
+const radiusValue = d => Number(d.Total_Sales)
 
 d3.json("https://raw.githubusercontent.com/techslides/D3-Maps/master/data/world/country/Bangladesh.topo.json").then((mapData) => {
-  d3.csv("./geo.csv").then((data) => {
+  d3.csv("./geo.csv").then((salesData) => {
     const areas = topojson.feature(mapData, mapData.objects.map);
-    // const 
 
+    radiusScale.domain([0, d3.max(salesData, radiusValue)]).range([0,5])
     const paths = g.selectAll("path").data(areas.features);
     paths
       .enter()
@@ -16,13 +18,12 @@ d3.json("https://raw.githubusercontent.com/techslides/D3-Maps/master/data/world/
       .attr("d", (d) => pathGenerator(d));
 
     g.selectAll("circle")
-      .data(areas.features)
+      .data(salesData)
       .enter()
       .append("circle")
       .style("fill", "#03a9f4")
-      .attr("cx", d => projection(d3.geoCentroid(d))[0])
-      .attr("cy", d => projection(d3.geoCentroid(d))[1])
-      .attr("r", 10)
-
+      .attr("cx", (d) => projection([Number(d.longitude)], Number(d.latitude))[0])
+      .attr("cy", (d) => projection([Number(d.longitude), Number(d.latitude)])[1])
+      .attr("r", d => radiusScale(radiusValue(d)));
   });
 });
