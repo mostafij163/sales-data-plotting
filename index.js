e1f4830d813bdb20c526c4c2a6d45090a9488d3b
mostafij163 <mostafijurrahman163@gmail.com>
2022-04-04 15:17:37 +0600

    change data and spike style

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const svg = d3.select("svg");
-const g = svg.append("g") .attr("class", "map-path");
+const g = svg.append("g").attr("class", "map-path");
 const width = +svg.attr("width");
 const height = +svg.attr("height");
 
@@ -9,7 +9,7 @@ function spike(length, width = 10) {
 
 const projection = d3
   .geoMercator()
-  .scale(60000)
+  .scale(15000)
   .center([90.56423035, 23.79456711])
   .translate([width / 2, height / 2]);
 const pathGenerator = d3.geoPath().projection(projection);
@@ -36,26 +36,68 @@ d3.json("https://raw.githubusercontent.com/techslides/D3-Maps/master/data/world/
     //   .attr("cy", (d) => projection([Number(d.longitude), Number(d.latitude)])[1])
     //   .attr("r", (d) => radiusScale(radiusValue(d)));
 
-    const formatedData = salesData.map((d) => ({
-      position: [projection([Number(d.longitude)], Number(d.latitude))[0], projection([Number(d.longitude), Number(d.latitude)])[1]],
-      TOTAL_Sales: d.TOTAL_Sales,
-      Consumer_Footfall: d.Consumer_Footfall,
-      LOW_Sales: d.LOW_Sales,
-      LOWPLUS_SALES: d.LOWPLUS_SALES,
-      VFM_Sales: d.VFM_Sales,
-      AP_Sales: d.AP_Sales,
-      P_Sales: d.P_Sales,
-      title: "dummy title",
-    }));
+    const formatedData1 = [
+      {
+        position: [projection([Number(90.42272949)], Number(23.78141022))[0], projection([Number(90.42272949), Number(23.78141022)])[1]],
+        TOTAL_Sales: 1.534269173,
+        title: "dummy title",
+      },
+    ];
+
+    const formatedData2 = [
+      {
+        position: [projection([Number(90.42272949)], Number(23.78141022))[0], projection([Number(90.42272949), Number(23.78141022)])[1]],
+        TOTAL_Sales: 2.534269173,
+        title: "dummy title",
+      },
+    ];
+
+    const formatedData3 = [
+      {
+        position: [projection([Number(90.42272949)], Number(23.78141022))[0], projection([Number(90.42272949), Number(23.78141022)])[1]],
+        TOTAL_Sales: 3.534269173,
+        title: "dummy title",
+      },
+    ];
+
+    const formatedData4 = [
+      {
+        position: [projection([Number(90.42272949)], Number(23.78141022))[0], projection([Number(90.42272949), Number(23.78141022)])[1]],
+        TOTAL_Sales: 4.534269173,
+        title: "dummy title",
+      },
+    ];
+
+    const formatedData5 = [
+      {
+        position: [projection([Number(90.42272949)], Number(23.78141022))[0], projection([Number(90.42272949), Number(23.78141022)])[1]],
+        TOTAL_Sales: 5.534269173,
+        title: "dummy title",
+      },
+    ];
 
     svg
       .append("g")
-      .attr("class", "total-sales")
+      .attr("class", "total-point1")
       .selectAll("path")
-      .data(formatedData.filter((d) => d.position).sort((a, b) => d3.ascending(a.position[1], b.position[1]) || d3.ascending(a.position[0], b.position[0])))
+      .data(formatedData1)
       .join("path")
       .attr("transform", (d) => `translate(${d.position})`)
-      .attr("d", (d) => spike(d3.scaleLinear([0, d3.max(formatedData, (d) => d.TOTAL_Sales)], [0, 200])(d.TOTAL_Sales)))
+      .attr("d", (d) => spike(d3.scaleLinear([0, d3.max(formatedData1, (d) => d.TOTAL_Sales)], [0, 180])(d.TOTAL_Sales)))
+      .append("title")
+      .text(
+        (d) => `${d.title}
+  ${d3.format(",.0f")(d.TOTAL_Sales)}`
+      );
+
+    svg
+      .append("g")
+      .attr("class", "total-point2")
+      .selectAll("path")
+      .data(formatedData2)
+      .join("path")
+      .attr("transform", (d) => `translate(${d.position})`)
+      .attr("d", (d) => spike(d3.scaleLinear([0, d3.max(formatedData2, (d) => d.TOTAL_Sales)], [0, 150])(d.TOTAL_Sales)))
       .append("title")
       .text(
         (d) => `${d.title}
@@ -64,17 +106,58 @@ d3.json("https://raw.githubusercontent.com/techslides/D3-Maps/master/data/world/
 
       svg
       .append("g")
-      .attr("class", "consumer-footfall")
+      .attr("class", "total-point3")
       .selectAll("path")
-      .data(formatedData.filter((d) => d.position).sort((a, b) => d3.ascending(a.position[1], b.position[1]) || d3.ascending(a.position[0], b.position[0])))
+      .data(formatedData3)
       .join("path")
       .attr("transform", (d) => `translate(${d.position})`)
-      .attr("d", (d) => spike(d3.scaleLinear([0, d3.max(formatedData, (d) => d.Consumer_Footfall)], [0, 50])(d.Consumer_Footfall)))
+      .attr("d", (d) => spike(d3.scaleLinear([0, d3.max(formatedData3, (d) => d.TOTAL_Sales)], [0, 120])(d.TOTAL_Sales)))
       .append("title")
       .text(
         (d) => `${d.title}
-  ${d3.format(",.0f")(d.Consumer_Footfall)}`
+  ${d3.format(",.0f")(d.TOTAL_Sales)}`
       );
+
+      svg
+      .append("g")
+      .attr("class", "total-point4")
+      .selectAll("path")
+      .data(formatedData4)
+      .join("path")
+      .attr("transform", (d) => `translate(${d.position})`)
+      .attr("d", (d) => spike(d3.scaleLinear([0, d3.max(formatedData4, (d) => d.TOTAL_Sales)], [0, 90])(d.TOTAL_Sales)))
+      .append("title")
+      .text(
+        (d) => `${d.title}
+  ${d3.format(",.0f")(d.TOTAL_Sales)}`
+      );
+
+      svg
+      .append("g")
+      .attr("class", "total-point5")
+      .selectAll("path")
+      .data(formatedData5)
+      .join("path")
+      .attr("transform", (d) => `translate(${d.position})`)
+      .attr("d", (d) => spike(d3.scaleLinear([0, d3.max(formatedData5, (d) => d.TOTAL_Sales)], [0, 60])(d.TOTAL_Sales)))
+      .append("title")
+      .text(
+        (d) => `${d.title}
+  ${d3.format(",.0f")(d.TOTAL_Sales)}`
+      );
+
+    //     svg
+    //     .append("g")
+    //     .attr("class", "consumer-footfall")
+    //     .selectAll("path")
+    //     .data(formatedData.filter((d) => d.position).sort((a, b) => d3.ascending(a.position[1], b.position[1]) || d3.ascending(a.position[0], b.position[0])))
+    //     .join("path")
+    //     .attr("transform", (d) => `translate(${d.position})`)
+    //     .attr("d", (d) => spike(d3.scaleLinear([0, d3.max(formatedData, (d) => d.Consumer_Footfall)], [0, 50])(d.Consumer_Footfall)))
+    //     .append("title")
+    //     .text(
+    //       (d) => `${d.title}
+    // ${d3.format(",.0f")(d.Consumer_Footfall)}`
+    //     );
   });
 });
-
